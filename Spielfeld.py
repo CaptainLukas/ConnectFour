8ae73ed0ff3010c2be20bc05e22e8872b27988eb
CaptainLukas <Lukas.Forsthuber@gmx.at>
2020-02-01 15:01:32 +0100

    documentation of the matchfield

--- Spielfeld.py
+++ Spielfeld.py
@@ -2,6 +2,24 @@ import Spielerfarbe
 class Spielfeld:
 
     # the matchfield
+    # the matchfield is stored into a list of Spielerfarbe
+    # the 6x7 field is stored in a straight list
+    # each column is 6 fields long
+    #
+    # the matchfield as it is shown to the user
+    #   1 2 3 4 5 6 7 columns
+    #  5[][][][][][][]
+    #  4[][][][][][][]
+    #  3[][][][][][][]
+    #  2[][][][][][][]
+    #  1[][][][][][][]
+    #  0[][][][][][][]
+    # rows
+    # the matchfield as it is stored in the list
+    #    row: 0  1  2  3  4  5  0  1  2  3  4  5  0  1  2  3  4  5  0  1  2  3  4  5  0  1  2  3  4  5  0  1  2  3  4  5  0  1  2  3  4  5
+    # column: 1  1  1  1  1  1  2  2  2  2  2  2  3  3  3  3  3  3  4  4  4  4  4  4  5  5  5  5  5  5  6  6  6  6  6  6  7  7  7  7  7  7
+    #   list: [] [] [] [] [] []|[] [] [] [] [] []|[] [] [] [] [] []|[] [] [] [] [] []|[] [] [] [] [] []|[] [] [] [] [] []|[] [] [] [] [] []
+    #  index: 0  1  2  3  4  5  6  7  8  9  10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41
     __field = list()
 
     # the players color
