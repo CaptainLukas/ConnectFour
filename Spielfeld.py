f2a0299a3f3ebf84de22983e82812a6e874ebfad
CaptainLukas <Lukas.Forsthuber@gmx.at>
2020-02-01 10:18:11 +0100

    Spielfeld dokumentiert

--- Spielfeld.py
+++ Spielfeld.py
@@ -1,10 +1,20 @@
 import Spielerfarbe
 class Spielfeld:
+
+    # the matchfield
     __field = list()
+
+    # the players color
     farbe = Spielerfarbe
-    def __init__(self):
+
+    # initialize a new instance of Spielfeld
+    def __init__(self, farbe):
         self._field= self.newField()
+        self.fatbe = farbe
         self.data=[]
+
+    # sets a new matchfield
+    # return:list returns an list with only Spielerfarbe.leer fields
     def newField(self):
         self.__field = list()
         i=0
@@ -13,6 +23,8 @@ class Spielfeld:
             i += 1
         return self.__field
 
+    # gets the matchfield if not None
+    # return:list returns the matchfield
     def getField(self):
         if self.__field == None:
             #Error werfen
@@ -20,13 +32,16 @@ class Spielfeld:
 
         return self.__field
 
-    def makeMove(self, row):
-        if (row > 7):
+    # this method adds a piece to the first free field in the selected column in the players color
+    # column:int [1,7] the column selected to to add a piece
+    # return:bool returns whether the move was valid
+    def makeMove(self, column):
+        if (column > 7 or column < 0):
             return False
-        if (self.__field[row*6-1] != Spielerfarbe.leer):
+        if (self.__field[column * 6 - 1] != Spielerfarbe.leer):
             return False
         else:
-            i = (row -1) * 6
+            i = (column - 1) * 6
             while i < 42:
                 if (self.__field[i]==Spielerfarbe.leer):
                     check = self.__changeField(i)
@@ -34,8 +49,11 @@ class Spielfeld:
                 i+=1
         return True
 
+    # this method sets a field to the players color
+    # index:int [0,41] the absolute index of the field to be set
+    # return:bool returns whether the change was done
     def __changeField(self,index):
-        if index > 41: return False
+        if index > 41 or index < 0: return False
         if self.__field[index] != Spielerfarbe.leer: return False
         self.__field[index] = self.farbe
         return True
