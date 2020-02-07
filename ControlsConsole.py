480c0a996dd3bfda323121bd704b0ac393dd9b60
CaptainLukas <Lukas.Forsthuber@gmx.at>
2020-02-07 17:45:36 +0100

    UserInterfaceConsole optimieren für Ausgabe, ControlsConsole funktioniert noch nicht

--- ControlsConsole.py
+++ ControlsConsole.py
@@ -1,8 +1,17 @@
 import keyboard
+import _thread
+import sys
+from msvcrt import getch
 #https://pypi.org/project/keyboard/
 #https://stackoverflow.com/questions/24072790/detect-key-press-in-python
+
+def keypress():
+    global char
+    char = getch()
+
 def GetUserinput():
-    keyboard.wait('1')
+    #keyboard.wait('1')
+
     while True:  # making a loop
         try:  # used try so that if user pressed other than the given key error will not be shown
             if keyboard.is_pressed('q'):  # if key 'q' is pressed
--- UserInterfaceConsole.py
+++ UserInterfaceConsole.py
@@ -1,12 +1,22 @@
+from Spielerfarbe import Spielerfarbe
 def PrintField(field):
     i = 5
     print('  1     2     3     4     5     6     7')
     while(i >=0):
         #print('['+str(i)+']' +'['+ str(i + 1 * 6)+']' + '['+str(i + 2 * 6)+ ']' + '[' + str(i + 3 * 6) + ']' + '[' + str(i + 4 * 6)+ ']' + '[' + str(i + 5 * 6) + ']' + '[' + str(i + 6 * 6) + ']')
-        print('[' +field[i].name+ ']'+ '['+field[i+(1*6)].name+']' + '['+field[i+(2*6)].name+']'+ '['+field[i+(3*6)].name+']'+ '['+field[i+(4*6)].name+']'+ '['+field[i+(5*6)].name+']'+ '['+field[i+(6*6)].name+']')
+        print('[' +__printName(field[i])+ ']'+ '['+field[i+(1*6)].name+']' + '['+field[i+(2*6)].name+']'+ '['+field[i+(3*6)].name+']'+ '['+field[i+(4*6)].name+']'+ '['+field[i+(5*6)].name+']'+ '['+field[i+(6*6)].name+']')
         i-=1
     return
 
+def __printName(farbe):
+    if farbe == Spielerfarbe.leer:
+        return ' '
+    elif farbe == Spielerfarbe.gelb:
+        return 'X'
+    elif farbe == Spielerfarbe.rot:
+        return 'O'
+    else:
+        return 'Fehler'
 def PrintWin():
     return
 
