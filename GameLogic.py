599f6c54bfda295618a92e74a1dc36b9aecc7535
CaptainLukas <Lukas.Forsthuber@gmx.at>
2020-02-12 16:52:50 +0100

    added code for the flow of the game

--- GameLogic.py
+++ GameLogic.py
@@ -1,35 +1,42 @@
 from Spielfeld import Spielfeld
 from Spielerfarbe import Spielerfarbe as sf
 import UserInterfaceConsole as ui
-import ControlsConsole
+import ControlsConsole as cc
+From Network import EnhancedNetwork
 
 class GameLogic:
 
     __matchfield = Spielfeld
+    __connection = EnhancedNetwork
 
-    #def __init__(self):
-    #    self.data=[]
+    def __init__(self):
+        self.__matchfield = Spielfeld()
+        self.__connection = EnhancedNetwork()
+        self.data=[]
 
     def startGame(self):
-        self.__matchfield = Spielfeld(sf.gelb)
+        #self.__matchfield = Spielfeld(sf.gelb)
         self.__matchfield.newField()
 
         self.testgame()
-        ControlsConsole.GetUserinput()
+        self.startConncetion()
+        
         return
 
     def testgame(self):
         ui.PrintField(self.__matchfield.getField())
         self.__matchfield.makeMove(3, sf.gelb)
-        ui.PrintField(self.__matchfield.getField())
         self.__matchfield.makeMove(4,sf.rot)
-        ui.PrintField(self.__matchfield.getField())
         self.__matchfield.makeMove(4, sf.gelb)
         self.__matchfield.makeMove(3, sf.rot)
-        self.__matchfield.makeMove(3, sf.gelb)
-        self.__matchfield.makeMove(4, sf.rot)
-        self.__matchfield.makeMove(5, sf.gelb)
-        ui.PrintField(self.__matchfield.getField())
-        self.__matchfield.makeMove(2, sf.rot)
         ui.PrintField(self.__matchfield.getField())
         return
+    
+    def startConnection():
+        ui.PrintGetIPInfo()
+        ip = cc.GetUserInputString()
+        ui.PrintGetPortInfo()
+        port = cc.GetUserInputString()
+        self.__conncetion.startNewConnection(ip,port,30)#30?
+        self.__connection.sendMessage('startGame')
+        
