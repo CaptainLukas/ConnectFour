d624dd753a904dc3694fd39db6d009bdaaaf7c75
CaptainLukas <Lukas.Forsthuber@gmx.at>
2020-02-20 18:21:00 +0100

    Documentation of Network.py

--- Network.py
+++ Network.py
@@ -3,22 +3,25 @@ import socket
 #https://wiki.python.org/moin/TcpCommunication
 #https://docs.python.org/3/howto/sockets.html
 
-# vl nur einfach die ausgewählte Spalte schicken und jeder schaut sich selbst an wie der move wird
+# NETWORK PROTOCOL
+# Header(CON4)|Payloadlength|E|Payload
+#ExampleMessage:
+# CON45EHello
 class EnhancedNetwork:
     __port=45688
-    __buffersize=256
-    __ip = ''
+    __ip = '192.168.1.86'
     __connection = socket
     __connectionStarted = False
 
-    def __init__(self,ip):
-        self.__ip = ip
+    def __init__(self):
         #AF_INET represents address and protocol family -> IPv4
         #SOCK_STREAM sets the socket type
         self.__connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         return
 
-    def connectToOther(self):
+    #connects to the other player
+    def connectToOther(self, ip):
+        self.__ip = ip
         if self.__connectionStarted : return
         try:
             self.__connection.connect((self.__ip, self.__port))
@@ -28,10 +31,13 @@ class EnhancedNetwork:
             print('Connection Failed')
         return
 
+    #waits for other player to connect
     def waitForConnection(self):
         if self.__connectionStarted: return
         try:
+            #bind socket to ipv4 address of pc
             self.__connection.bind((socket.gethostname(), self.__port))
+            #wait for one client to connect
             self.__connection.listen(1)
             (self.__connection, addr) = self.__connection.accept()
             print('Connected')
@@ -51,7 +57,9 @@ class EnhancedNetwork:
     def sendMessage(self, message):
         if not self.__connectionStarted: return
         try:
+            #add header and length info
             message = 'CON4'+ str(len(message.encode('utf-8'))) + 'E'+ message
+            #send complete message
             self.__connection.send(message.encode('utf-8'))
         except:
             print('Sending message failed.')
@@ -61,6 +69,8 @@ class EnhancedNetwork:
         chunks = []
         bytes_recd = 0
         messagelen = 0
+
+        #this loop checks for the header at the beginning of the message
         while(True):
             chunk = self.__connection.recv(1)
             if chunk != bytes('C','utf-8'):
@@ -77,6 +87,7 @@ class EnhancedNetwork:
             break
 
         bytenumbers=b''
+        #reads the length of the payload
         while(chunk != bytes('E','utf-8')):
             chunk = self.__connection.recv(1)
             if chunk == bytes('E','utf-8'):
@@ -84,5 +95,6 @@ class EnhancedNetwork:
             bytenumbers = bytenumbers+ chunk
 
         number = int(bytenumbers.decode('utf-8'))
+        #read payload
         chunk = self.__connection.recv(number)
         return chunk.decode('utf-8')
