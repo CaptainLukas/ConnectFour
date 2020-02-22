21a6740a6a01b24ec130bd1d2e08db5f175421ce
CaptainLukas <Lukas.Forsthuber@gmx.at>
2020-02-22 09:00:08 +0100

    Added file ControlsGUI, listed possible solution

---
+++ ControlsGUI.py
@@ -0,0 +1,5 @@
+#https://wiki.python.org/moin/GuiProgramming
+#choose a GUIform, some possible solutions listed below
+#TkInter - Python's "standard" GUI library
+#guizero - The aim of guizero is to make the process of creating simple GUIs quick, accessible and understandable for children. It is a Python 3 library based on Tkinter, for creating simple GUIs.
+#PyGame - Primarily an introduction to game programming. Only supports one window.(Python 2 & 3)
